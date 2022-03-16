84bb6f9ff3fcba7993c2c47b5ad23aa0503d67cb
rvnagmode <101249215+rvnagmode@users.noreply.github.com>
2022-03-16 10:53:24 +0530

    Add project files.

---
+++ Day3/Product.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+
+namespace Day3
+{
+    public class Product
+    {
+       private int pcode;
+       private string pname;
+       private double price;
+       private double dis;
+        public Product(int pcode,string pname,double price)
+        {
+            this.pcode = pcode;
+            this.pname = pname;
+            this.price = price;
+        }
+        public void Discount()
+        {
+            if(price>5000)
+            {
+                dis = 5000 - (5000 * 0.10);
+               
+                Console.WriteLine("Discounted price = " + dis);
+            }
+            else
+                Console.WriteLine("final price = " + price);
+            
+        }
+    }
+}
---
+++ Day3/Program.cs
@@ -0,0 +1,13 @@
+﻿using Day3;
+
+namespace Day3
+{ 
+    public class Testing
+    {
+        static void Main(string[] args)
+        {
+            Product p = new Product(1025,"headphones",6000);
+            p.Discount();
+        }
+    }
+}
